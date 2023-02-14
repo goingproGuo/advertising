3b1b94bba428d1bc2cee1e0764594e1be246b090
dpx-express <guoyuyang@dpx-express.com>
2023-02-14 10:07:05 +0800

    Initial commit

---
+++ src/test/java/com/qsc/advertising/AdvertisingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsc.advertising;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
